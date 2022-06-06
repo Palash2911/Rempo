371ab1357688409576d8a3ad49fb87a9e59ea715
Palash Rambhia <palashrambhia2@gmail.com>
2022-06-07 00:05:43 +0530

    Bookmark Page query added

--- app/src/main/java/com/godspeed/propmart/ui/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/godspeed/propmart/ui/bookmark/BookmarkFragment.kt
@@ -1,20 +1,27 @@
 package com.godspeed.propmart.ui.Bookmarks
 
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.godspeed.propmart.Bottomtab
 import com.godspeed.propmart.databinding.FragmentBookmarkBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class BookmarksFragment : Fragment() {
 
+    private val db = Firebase.firestore
     private var _binding: FragmentBookmarkBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,10 +35,12 @@ class BookmarksFragment : Fragment() {
         _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-//        val textView: TextView = binding.textBookmarks
-//        BookmarksViewModel.text.observe(viewLifecycleOwner) {
-//            textView.text = it
-//        }
+//        db.collection("Users").document("sample_uid")
+//            .collection("saved").get().addOnSuccessListener { snapshot ->
+//                for (layout in snapshot) {
+//                    _binding!!.textView6.text = layout["time"].toString()
+//                }
+//            }
         return root
     }
 
