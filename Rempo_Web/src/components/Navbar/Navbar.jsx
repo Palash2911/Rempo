41eaf39a890b6c51d375b8c4b6e53aec70a797a3
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-10-31 23:05:53 +0530

    navbar sudo code added

--- Rempo_Web/src/components/Navbar/Navbar.jsx
+++ Rempo_Web/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Navbar.module.css";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className={classes.navbar}>
+        <div className={classes.nav_container}>
+          <NavLink exact to="/" className={classes.nav_logo}>
+            CodeBucks
+          </NavLink>
+
+          <ul
+            className={
+              click ? `${classes.nav_menu} ${classes.active}` : "nav_menu"
+            }
+          >
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/about"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/blog"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Blog
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/contact"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Contact Us
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
 
 export default Navbar;
