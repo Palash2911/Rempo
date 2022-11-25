a7d053894dc3f3dcc219b3e75d26549d34f3910c
Palash Rambhia <palashrambhia2@gmail.com>
2022-11-25 19:48:29 +0530

    otp done

--- Rempo_Web/src/components/Login/Login.jsx
+++ Rempo_Web/src/components/Login/Login.jsx
@@ -1,23 +1,78 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom'
 import { Button, Card } from "../Ui";
 import classes from "./Login.module.css";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import useScreenType from "react-screentype-hook";
+import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
+import app from "../firebase_config"
+
+
+const auth = getAuth(app);
 
 const Login = () => {
   let switcher = 1;
   const screenType = useScreenType();
+  let histo = useNavigate();
+
+  const onCaptchVerify=() =>{
+    console.log("signined")
+    window.recaptchaVerifier = new RecaptchaVerifier
+    ("recaptcha-container", 
+    {
+      size: "invisible",
+      callback: (response) => {
+        onSignInSubmit()
+    // reCAPTCHA solved, allow signInWithPhoneNumber.
+    },
+  }, 
+  auth);
+  }
 
   const [state, setState] = useState({ phone: 0 });
+  const [otpstate, otpsetState] = useState({ otp: '' });
+  const [flagstate, setFlag] = useState({flag: 0});
   // To check whether the number is correct or not.
   useEffect(() => {
     console.log(state.phone);
+    console.log(otpstate.otp);
   });
+
+  const onSignInSubmit=() =>  {
+    onCaptchVerify();
+    setFlag({flag: 1})
+    const phoneNumber = "+91" + state.phone.toString().substring(2); 
+    const appVerifier = window.recaptchaVerifier;
+    console.log("fuck:"+appVerifier)
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+    .then((confirmationResult) => {
+      window.confirmationResult = confirmationResult;
+      // ...
+    }).catch((error) => {
+      // Error; SMS not sent
+      // ...
+      console.log(error.toString)
+    });
+  }
+
+  const onVerifyCode=() =>{
+    window.confirmationResult.confirm(otpstate.otp).then((result) => {
+      // User signed in successfully.
+      const user = result.user;
+      histo("/")
+      // ...
+    }).catch((error) => {
+      // User couldn't sign in (bad verification code?)
+      // ...
+    });
+  }
+
   return (
     <>
       <div className={classes.container}>
-        <Card
+      <div id="recaptcha-container"></div>
+       {flagstate.flag==0 ? <Card
           width={screenType.isMobile ? "90vw" : "35vw"}
           height="70vh"
           radius="24px"
@@ -35,12 +90,25 @@ const Login = () => {
                 />
               </div>
             </div>
-            <Button label="Send OTP" padding="0.5em 7em" radius="24px" />
-            <p>
-              Don&apos;t have an account ? <span>Create Account</span>
-            </p>
+            <Button label="Send OTP" padding="0.5em 7em" radius="24px" onClick={onSignInSubmit} />
           </div>
-        </Card>
+        </Card>: 
+        <Card
+        width={screenType.isMobile ? "90vw" : "35vw"}
+        height="70vh"
+        radius="24px"
+      >
+        <div className={classes.form_container}>
+          <h1>Login</h1>
+          <div className={classes.outerContainer}>
+            <p>OTP</p>
+            <div className={classes.PhoneInputContainer}>
+              <input type="number" value={otpstate.otp} onChange={(e) => otpsetState({ otp:e.target.value })} />
+            </div>
+          </div>
+          <Button label="Verify OTP" padding="0.5em 7em" radius="24px" onClick={onVerifyCode}/>
+        </div>
+      </Card>}
       </div>
     </>
   );
---
+++ Rempo_Web/src/components/firebase_config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCkP5T1MV37qYoO8ovcUpNQTyp0JucRY64",
+  authDomain: "remp-e7e85.firebaseapp.com",
+  projectId: "remp-e7e85",
+  storageBucket: "remp-e7e85.appspot.com",
+  messagingSenderId: "337409210695",
+  appId: "1:337409210695:web:9a18b28c6fff3bf6fbf462",
+  measurementId: "G-V6YSWZQR3Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
