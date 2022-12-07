e64f26a1d9228ab76d18d137dc0cbceb86991bf6
Palash Rambhia <palashrambhia2@gmail.com>
2022-12-08 02:57:20 +0530

    Half Firebase Done

--- Rempo_Web/src/components/Layout/Form3.jsx
+++ Rempo_Web/src/components/Layout/Form3.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useContext  } from "react";
 import classes from "./Layout.module.css";
 import { Button, Card, Input } from "../Ui";
 import useScreenType from "react-screentype-hook";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app, { db } from "../firebase_config";
+import { collection, doc, setDoc, getDoc, addDoc } from "firebase/firestore";
+import FormContext from "./formContenxt/formContext";
+
+const auth = getAuth(app);
 
 const inputArr = [
   {
@@ -14,6 +20,46 @@ const inputArr = [
 const Form3 = () => {
   const [arr, setArr] = useState(inputArr);
   const screenType = useScreenType();
+
+  const {
+    owner,
+    setOwner,
+    category,
+    setCategory,
+    desc,
+    setDesc,
+    district,
+    setDistrict,
+    taluka,
+    setTaluka,
+    village,
+    setVillage,
+  } = useContext(FormContext);
+
+  
+  const handleClick = async (e) => {
+    e.preventDefault();
+      try {
+        const docid = await addDoc(collection(db, "Layouts"), {
+          address: district,
+          availabelPlots: 2,
+          district: district,
+          layoutId: "adsf",
+          location: "1234-5678-9000",
+          sellerId: auth.currentUser.uid,
+          sellerName: owner,
+          soldPlots: 1,
+          State: "Goa",
+          taluka: taluka,
+          title: "TITLE",
+          totalPlots: 3,
+        });
+        console.log("asdf", docid.id)
+      } catch (e) {
+        console.log("Error", e);
+        alert(e);
+      }
+  };
 
   const addInput = () => {
     setArr((s) => {
@@ -127,6 +173,9 @@ const Form3 = () => {
       </div>
       <Button label="Add Plot" type="1" onClick={addInput}>
         Add Plot
+      </Button>
+      <Button label="NEXT" type="1" onClick={handleClick}>
+        Next
       </Button>
     </>
   );
