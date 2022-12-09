ff816c85f73544acfd770673367060eb2ddd8b60
Palash Rambhia <palashrambhia2@gmail.com>
2022-12-09 23:57:18 +0530

    Project 99% Done

--- Rempo_Web/src/components/Landing/Landing.jsx
+++ Rempo_Web/src/components/Landing/Landing.jsx
@@ -3,9 +3,24 @@ import classes from "./Landing.module.css";
 import landing from "../../assets/landing.png";
 import Card from "../Ui/Card/Card";
 import { Button } from "../Ui";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import app  from "../firebase_config";
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth(app);
 
 const Landing = () => {
+  let histo = useNavigate();
+  const handleClick = async () =>{
+    if(auth.currentUser==null)
+    {
+      histo("/login")
+    }
+    else
+    {
+      histo("/layout")
+    }
+  }
   return (
     <>
       <div className={classes.landing_container}>
@@ -14,9 +29,7 @@ const Landing = () => {
           <Card width="50vw" height="200px" radius="15px">
             <div className={classes.btn_container}>
               <Button type="1" label="List a Plot"></Button>
-              <Link to="/layout">
-                <Button type="2" filled label="List a Layout" />
-              </Link>
+              <Button type="2" filled label="List a Layout" onClick={handleClick}/>
             </div>
           </Card>
         </div>
